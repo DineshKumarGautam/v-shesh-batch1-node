81ad752e903d55d70b4e5a323eac143d0eee934b
DineshKumarGautam <gautamdineshkumar01@gmail.com>
2021-10-07 22:25:35 +0530

    get

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
 const app = express();
 const http = require('http').createServer(app);
@@ -7,8 +7,24 @@ const http = require('http').createServer(app);
 app.use(bodyParser.json());
 app.use(express.json());
 
+const userList = [
+{
+    name : "Dinesh",
+    age : 27
+},
+{
+    name : "DineshKumar",
+    age : 28
+},
+{
+    name : "Dk",
+    age : 29
+},
+]
 
-
+app.get("/api/users",(request, responce) => {
+    responce.status(200),send(userList);
+});
 
 const port = process.env.PORT || 8080;
 http.listen(port, () => {
