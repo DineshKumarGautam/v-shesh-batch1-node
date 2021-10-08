403ae54a9216a739587b2d0c71f44b8a54ca2ffd
DineshKumarGautam <gautamdineshkumar01@gmail.com>
2021-10-08 09:21:52 +0530

    get

--- API.js
+++ API.js
@@ -1,4 +1,4 @@
-API
+/*API
 1. GET - Getting information from Server
 2. POST - Sending information to Server(Creation)
 3. PUT - Sending information to Server(Updation)
@@ -7,4 +7,4 @@ API
 Error code : 
 2X - 200, 201, 202, 203 (success)
 4X - 401, 402, 404 (client side Error)
-5X - 501, 502 (Server side Error )
+5X - 501, 502 (Server side Error )*/
--- server.js
+++ server.js
@@ -9,21 +9,68 @@ app.use(express.json());
 
 const userList = [
 {
-    name : "Dinesh",
-    age : 27
+    name : "Dinesh", 
+    age : 28,
+    id : 1
 },
 {
-    name : "DineshKumar",
-    age : 28
-},
-{
-    name : "Dk",
-    age : 29
+    name : "Kumar",
+    age : 29,
+    id = 2
 },
 ]
 
-app.get("/api/users",(request, responce) => {
-    responce.status(200),send(userList);
+app.post("/api/user/create", (request, responce) => {
+    let user = request.body;
+    user.id = userList.length + 1;
+    
+    userList.push(user);
+    responce.status(200).send({
+        message : "successfully created the user profile"
+
+    })
+});
+
+app.put("/api/user/edit/:id", (request, responce) => {
+    let id = request.params.id;
+    let index = userlist.findIndex((value) => {
+        return value.id == id;
+});
+
+if(index < 0){
+    responce.status(401).send({
+        message : "Invalid ID number"
+    });
+    return;
+}
+
+userList[index].name = request.body.name;
+userList[index].age = request.body.age;
+
+responce.status(200).send({
+    mesage : "Successfuly Updated the user information"
+ })
+ 
+});
+
+
+app.delete("/api/user/delete/:id", (request,responce) => {
+    let id = request.params.id;
+    let index = userList.findIndex((value) => {
+    return value.id == id;
+    });
+   
+    if(index < 0){
+        responce.status(401).send({
+            message : "Invalid ID number"
+        });
+        return;
+}
+
+userList.splice(index,1);
+responce.status(200).send({
+    message : "User profile hasbeen deleted successfully"
+})
 });
 
 const port = process.env.PORT || 8080;
